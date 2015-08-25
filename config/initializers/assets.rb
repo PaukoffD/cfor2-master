0dd832ebc23715b759602cd7a18261f6d8d0fecd
xuliaP <paukov2967@gmail.com>
2015-08-25 16:50:04 +0300

    skeditor
    skeditor

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,3 +11,4 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( forem.css forem.js )
 #Rails.application.config.assets.precompile += %w( jquery_ujs.js )
+Rails.application.config.assets.precompile += %w( ckeditor/* )
